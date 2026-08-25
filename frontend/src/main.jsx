17d543cbe55cefbf41ec47c9e20f3ac329f9178c
SONU KUMAR GUPTA <sonugupta59160@gmail.com>
2026-08-25 07:51:09 +0530

    Rename App import from App.jsx to app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './style.css'
 
 // Entry point for EarthNeer React application
